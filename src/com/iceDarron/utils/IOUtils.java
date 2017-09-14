eb9cd900dc7655643791e2db05c04468b6b5a91b
IceDarron <darron_r@163.com>
2017-09-14 22:50:08 +0800

    增加图片二进制转换方法

--- src/com/iceDarron/utils/IOUtils.java
+++ src/com/iceDarron/utils/IOUtils.java
@@ -1,5 +1,7 @@
 package com.iceDarron.utils;
 
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.FileImageOutputStream;
 import java.io.*;
 
 public class IOUtils {
@@ -39,5 +41,54 @@ public class IOUtils {
                 e.printStackTrace();
             }
         }
+    }
+
+    //图片到byte数组
+    public static byte[] image2byte(String path){
+        byte[] data = null;
+        FileImageInputStream input = null;
+        try {
+            input = new FileImageInputStream(new File(path));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int numBytesRead = 0;
+            while ((numBytesRead = input.read(buf)) != -1) {
+                output.write(buf, 0, numBytesRead);
+            }
+            data = output.toByteArray();
+            output.close();
+            input.close();
+        }
+        catch (FileNotFoundException ex1) {
+            ex1.printStackTrace();
+        }
+        catch (IOException ex1) {
+            ex1.printStackTrace();
+        }
+        return data;
+    }
+
+    //byte数组到图片
+    public static void byte2image(byte[] data,String path){
+        if(data.length<3||path.equals("")) return;
+        try{
+            FileImageOutputStream imageOutput = new FileImageOutputStream(new File(path));
+            imageOutput.write(data, 0, data.length);
+            imageOutput.close();
+            System.out.println("Make Picture success,Please find image in " + path);
+        } catch(Exception ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        byte[] bytes = image2byte("H:\\Jetbrains\\Idea\\Workspace\\Algorithms\\src\\com\\iceDarron\\resource\\image\\ChMkJ1bxATiIVlzmAAlJIHaXmLkAAOTUgN32YEACUk4716.jpg");
+        System.out.println(bytes.length);
+//        for (int i = 0; i < bytes.length ; i++) {
+//            System.out.print(bytes[i]);
+//        }
+
+        byte2image(bytes, "H:\\Jetbrains\\Idea\\Workspace\\Algorithms\\src\\com\\iceDarron\\resource\\image\\Copy_ChMkJ1bxATiIVlzmAAlJIHaXmLkAAOTUgN32YEACUk4716.jpg");
     }
 }
