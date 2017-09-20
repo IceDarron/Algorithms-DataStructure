d2a49ef6d5890e36103193118a3f62c5fa6dde02
IceDarron <darron_r@163.com>
2017-09-20 22:53:01 +0800

    增加排序抽象类中对入参为Integer[]类型的处理

--- src/com/iceDarron/deepSort/abstra/AbstraDeepSort.java
+++ src/com/iceDarron/deepSort/abstra/AbstraDeepSort.java
@@ -4,25 +4,34 @@ import com.iceDarron.deepSort.interf.IDeepSort;
 
 import java.util.List;
 
-public abstract class AbstraDeepSort implements IDeepSort{
+public abstract class AbstraDeepSort implements IDeepSort {
 
     @Override
     public abstract void sort(int[] intArray);
 
-    public void sort(String[] strArray) {
-        int length = strArray.length;
+    public void sort(String[] array) {
+        int length = array.length;
         int[] intArray = new int[length];
         for (int i = 0; i < length; i++) {
-            intArray[i] = Integer.parseInt(strArray[i]);
+            intArray[i] = Integer.parseInt(array[i]);
         }
         sort(intArray);
     }
 
-    public void sort(List<?> listArray) {
-        int size = listArray.size();
+    public void sort(List<?> array) {
+        int size = array.size();
         int[] intArray = new int[size];
         for (int i = 0; i < size; i++) {
-            intArray[i] = Integer.parseInt((String) listArray.get(i));
+            intArray[i] = Integer.parseInt((String) array.get(i));
+        }
+        sort(intArray);
+    }
+
+    public void sort(Integer[] array) {
+        int length = array.length;
+        int[] intArray = new int[length];
+        for (int i = 0; i < length; i++) {
+            intArray[i] = array[i];
         }
         sort(intArray);
     }
