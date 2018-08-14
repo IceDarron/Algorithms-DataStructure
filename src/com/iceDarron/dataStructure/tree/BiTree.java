2e529dabc90ae4a8b1b93bac2384433759b5bb47
rxn <darron_r@163.com>
2018-08-14 15:58:14 +0800

    补充相关计算

--- src/com/iceDarron/dataStructure/tree/BiTree.java
+++ src/com/iceDarron/dataStructure/tree/BiTree.java
@@ -6,23 +6,28 @@ import java.util.Stack;
 
 /**
  * 二叉树相关
- *
- * 创建树，先中后序遍历
- *
+ * <p>
+ * 先（根>左>右）中（左>根>右）后（左>右>根）序遍历
  */
 public class BiTree {
 
-	public BTNode root;
+    public BTNode root;
 
-	public BiTree(BTNode root) {
-		this.root = root;
-	}
+    public BiTree(BTNode root) {
+        this.root = root;
+    }
 
-	public BTNode getRoot() {
-		return this.root;
-	}
+    public BTNode getRoot() {
+        return this.root;
+    }
 
-    /** 构造树 */
+    /**
+     * 构造树
+     * 8
+     * 4       7
+     * 2    3   n    6
+     * n  1           5  n
+     */
     public static BTNode init() {
         BTNode a = new BTNode(1);
         BTNode b = new BTNode(2, null, a);
@@ -35,128 +40,181 @@ public class BiTree {
         return h; // root
     }
 
-	/**
+    /**
+     * 打印树
+     */
+    public void printTree(BTNode root) {
+
+    }
+
+    /**
      * 访问节点
      */
-	public void visit(BTNode p) {
-		System.out.print(p.getVal() + " ");
-	}
+    public void visit(BTNode node) {
+        System.out.print(node.getVal() + " ");
+    }
 
-	/**
+    /**
      * 递归实现前序遍历
      */
-	public void preOrder(BTNode T) {
-		if (T != null) {
-			visit(T);
-			preOrder(T.getLeft());
-			preOrder(T.getRight());
-		}
-	}
+    public void preOrder(BTNode node) {
+        if (node != null) {
+            visit(node);
+            preOrder(node.getLeft());
+            preOrder(node.getRight());
+        }
+    }
 
-	/**
+    /**
      * 递归实现中序遍历
      */
-	public void inOrder(BTNode T) {
-		if (T != null) {
-			inOrder(T.getLeft());
-			visit(T);
-			inOrder(T.getRight());
-		}
-	}
+    public void inOrder(BTNode node) {
+        if (node != null) {
+            inOrder(node.getLeft());
+            visit(node);
+            inOrder(node.getRight());
+        }
+    }
 
-	/**
+    /**
      * 递归实现后序遍历
      */
-	public void postOrder(BTNode T) {
-		if (T != null) {
-			postOrder(T.getLeft());
-			postOrder(T.getRight());
-			visit(T);
-		}
-	}
+    public void postOrder(BTNode node) {
+        if (node != null) {
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            visit(node);
+        }
+    }
 
-	/**
+    /**
      * 非递归实现前序遍历
      */
-	public void iterativePreOrder(BTNode T) {
-		Stack<BTNode> stack = new Stack<>();
-		while (T != null || !stack.isEmpty()) {
-			if (T != null) {
-				visit(T);
-				if (T.getRight() != null)
-					stack.push(T.getRight());
-				T = T.getLeft();
-			} else {
-				T = stack.pop();
-			}
-		}
-	}
+    public void iterativePreOrder(BTNode node) {
+        Stack<BTNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                visit(node);
+                if (node.getRight() != null)
+                    stack.push(node.getRight());
+                node = node.getLeft();
+            } else {
+                node = stack.pop();
+            }
+        }
+    }
 
-	/**
+    /**
      * 非递归实现中序遍历
      */
-	public void iterativeInOrder(BTNode T) {
-		Stack<BTNode> stack = new Stack<>();
-		while (T != null || !stack.isEmpty()) {
-			if (T != null) {
-				stack.push(T);
-				T = T.getLeft();
-			} else {
-				T = stack.pop();
-				visit(T);
-				T = T.getRight();
-			}
-		}
-	}
+    public void iterativeInOrder(BTNode node) {
+        Stack<BTNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                node = node.getLeft();
+            } else {
+                node = stack.pop();
+                visit(node);
+                node = node.getRight();
+            }
+        }
+    }
 
-	/**
+    /**
      * 非递归实现后序遍历
      */
-	public void iterativePostOrder(BTNode T) {
-		Stack<BTNode> stack = new Stack<>();
-		Stack<Integer> helper = new Stack<>();
-		while (T != null || !stack.isEmpty()) {
-			if (T != null) {
-				stack.push(T);
-				helper.push(0);
-				T = T.getLeft();
-			} else {
-				int a = helper.pop();
-				if (a == 0) {
-					helper.push(1);
-					T = stack.peek().getRight();
-				} else {
-					T = stack.pop();
-					visit(T);
-					T = null;
-				}
-			}
-		}
-	}
+    public void iterativePostOrder(BTNode node) {
+        Stack<BTNode> stack = new Stack<>();
+        Stack<Integer> helper = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                stack.push(node);
+                helper.push(0);
+                node = node.getLeft();
+            } else {
+                int a = helper.pop();
+                if (a == 0) {
+                    helper.push(1);
+                    node = stack.peek().getRight();
+                } else {
+                    node = stack.pop();
+                    visit(node);
+                    node = null;
+                }
+            }
+        }
+    }
 
-	/**
+    /**
      * 层次遍历
      */
-	public void levelOrder(BTNode root) {
-		if (root == null) {
-			return;
-		}
-		Queue<BTNode> queue = new LinkedList<>();
-		queue.offer(root);
-		BTNode tmp = null;
-		while (!queue.isEmpty()) {
-			tmp = queue.poll();
-			visit(tmp);
-			if (tmp.getLeft() != null)
-				queue.offer(tmp.getLeft());
-			if (tmp.getRight() != null)
-				queue.offer(tmp.getRight());
-		}
-	}
+    public void levelOrder(BTNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<BTNode> queue = new LinkedList<>();
+        queue.offer(root);
+        BTNode tmp = null;
+        while (!queue.isEmpty()) {
+            tmp = queue.poll();
+            visit(tmp);
+            if (tmp.getLeft() != null)
+                queue.offer(tmp.getLeft());
+            if (tmp.getRight() != null)
+                queue.offer(tmp.getRight());
+        }
+    }
+
+    /**
+     * 计算二叉树结点
+     * <p>
+     * 用递归，每一个函数往左边和右边（顺序无关），两个方向探索，然后每探索成功一次，说明有一个结点，+1。出口为结点为空。
+     */
+    public int countNodes(BTNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return countNodes(node.getLeft()) + countNodes(node.getRight()) + 1;
+        }
+    }
+
+    /**
+     * 计算二叉树深度
+     * <p>
+     * 用递归，向左右探索（顺序无关），返回的值是二叉树的深度。
+     * <p>
+     * 分治思想，在左边和右边的子树那个深度大，那么就选那个深度大的+1 。出口为结点为空。
+     */
+    public int deepth(BTNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            int left = deepth(node.getLeft());
+            int right = deepth(node.getRight());
+            if (left > right)
+                return left + 1;
+            else
+                return right + 1;
+        }
+    }
 
 
-
-
+    /**
+     * 计算二叉树的叶子数目
+     *
+     * 递归，和探索结点一样，左右探索（顺序无关），结点左右子树皆为空，才是叶子。
+     *
+     * 出口两个，结点为空返回0，结点的左右子树为空返回1。
+     */
+    public int leafCount(BTNode node) {
+        if (node == null)
+            return 0;
+        else if (node.getLeft() == null && node.getRight() == null)
+            return 1;
+        else
+            return leafCount(node.getLeft()) + leafCount(node.getRight());
+    }
 
 }
 
@@ -164,70 +222,82 @@ public class BiTree {
  * 二叉树数据结构
  */
 class BTNode {
-	private int val;
-	private BTNode left, right;
+    private int val;
+    private BTNode left, right;
 
-	public BTNode(int val) {
-		this(val, null, null);
-	}
+    public BTNode(int val) {
+        this(val, null, null);
+    }
 
-	public BTNode(int val, BTNode left, BTNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
+    public BTNode(int val, BTNode left, BTNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
-	public int getVal() {
-		return val;
-	}
+    public int getVal() {
+        return val;
+    }
 
-	public void setVal(char key) {
-		this.val = key;
-	}
+    public void setVal(char key) {
+        this.val = key;
+    }
 
-	public BTNode getLeft() {
-		return left;
-	}
+    public BTNode getLeft() {
+        return left;
+    }
 
-	public void setLeft(BTNode left) {
-		this.left = left;
-	}
+    public void setLeft(BTNode left) {
+        this.left = left;
+    }
 
-	public BTNode getRight() {
-		return right;
-	}
+    public BTNode getRight() {
+        return right;
+    }
 
-	public void setRight(BTNode right) {
-		this.right = right;
-	}
+    public void setRight(BTNode right) {
+        this.right = right;
+    }
 }
 
 
+/**
+ * 测试类
+ */
 class TestBiTree {
 
     public static void main(String[] args) {
         BiTree tree = new BiTree(BiTree.init());
 
-        System.out.println("Pre-Order:");
-        tree.preOrder(tree.getRoot());
-        System.out.println();
-        tree.iterativePreOrder(tree.getRoot());
-        System.out.println();
+//        System.out.println("Pre-Order:");
+//        tree.preOrder(tree.getRoot());
+//        System.out.println();
+//        tree.iterativePreOrder(tree.getRoot());
+//        System.out.println();
 
-        System.out.println("In-Order:");
-        tree.inOrder(tree.getRoot());
-        System.out.println();
-        tree.iterativeInOrder(tree.getRoot());
-        System.out.println();
+//        System.out.println("In-Order:");
+//        tree.inOrder(tree.getRoot());
+//        System.out.println();
+//        tree.iterativeInOrder(tree.getRoot());
+//        System.out.println();
 
-        System.out.println("Post-Order:");
-        tree.postOrder(tree.getRoot());
-        System.out.println();
-        tree.iterativePostOrder(tree.getRoot());
-        System.out.println();
+//        System.out.println("Post-Order:");
+//        tree.postOrder(tree.getRoot());
+//        System.out.println();
+//        tree.iterativePostOrder(tree.getRoot());
+//        System.out.println();
 
-        System.out.println("Level-Order:");
-        tree.levelOrder(tree.getRoot());
-        System.out.println();
+//        System.out.println("Level-Order:");
+//        tree.levelOrder(tree.getRoot());
+//        System.out.println();
+
+//        System.out.println("Count-Nodes:");
+//        System.out.println(tree.countNodes(tree.root));
+
+//        System.out.println("Deepth:");
+//        System.out.println(tree.deepth(tree.root));
+
+        System.out.println("Leaf-Count:");
+        System.out.println(tree.leafCount(tree.getRoot()));
     }
 }
