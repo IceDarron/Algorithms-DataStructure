8631cab7ddb0a50971dfa493a450ae48a1841ac8
rxn <darron_r@163.com>
2018-08-14 14:47:26 +0800

    整理

--- src/com/iceDarron/dataStructure/tree/BiTree.java
+++ src/com/iceDarron/dataStructure/tree/BiTree.java
@@ -4,25 +4,48 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-/** 二叉树 */
+/**
+ * 二叉树相关
+ *
+ * 创建树，先中后序遍历
+ *
+ */
 public class BiTree {
-	protected BTNode root;
+
+	public BTNode root;
 
 	public BiTree(BTNode root) {
 		this.root = root;
 	}
 
 	public BTNode getRoot() {
-		return root;
+		return this.root;
 	}
 
-	/** 访问节点 */
-	public static void visit(BTNode p) {
+    /** 构造树 */
+    public static BTNode init() {
+        BTNode a = new BTNode(1);
+        BTNode b = new BTNode(2, null, a);
+        BTNode c = new BTNode(3);
+        BTNode d = new BTNode(4, b, c);
+        BTNode e = new BTNode(5);
+        BTNode f = new BTNode(6, e, null);
+        BTNode g = new BTNode(7, null, f);
+        BTNode h = new BTNode(8, d, g);
+        return h; // root
+    }
+
+	/**
+     * 访问节点
+     */
+	public void visit(BTNode p) {
 		System.out.print(p.getVal() + " ");
 	}
 
-	/** 递归实现前序遍历 */
-	protected static void preOrder(BTNode T) {
+	/**
+     * 递归实现前序遍历
+     */
+	public void preOrder(BTNode T) {
 		if (T != null) {
 			visit(T);
 			preOrder(T.getLeft());
@@ -30,8 +53,10 @@ public class BiTree {
 		}
 	}
 
-	/** 递归实现中序遍历 */
-	protected static void inOrder(BTNode T) {
+	/**
+     * 递归实现中序遍历
+     */
+	public void inOrder(BTNode T) {
 		if (T != null) {
 			inOrder(T.getLeft());
 			visit(T);
@@ -39,8 +64,10 @@ public class BiTree {
 		}
 	}
 
-	/** 递归实现后序遍历 */
-	protected static void postOrder(BTNode T) {
+	/**
+     * 递归实现后序遍历
+     */
+	public void postOrder(BTNode T) {
 		if (T != null) {
 			postOrder(T.getLeft());
 			postOrder(T.getRight());
@@ -48,8 +75,10 @@ public class BiTree {
 		}
 	}
 
-	/** 非递归实现前序遍历-我的代码 */
-	protected static void iterativePreOrder(BTNode T) {
+	/**
+     * 非递归实现前序遍历
+     */
+	public void iterativePreOrder(BTNode T) {
 		Stack<BTNode> stack = new Stack<>();
 		while (T != null || !stack.isEmpty()) {
 			if (T != null) {
@@ -63,8 +92,10 @@ public class BiTree {
 		}
 	}
 
-	/** 非递归实现中序遍历-我的代码 */
-	protected static void iterativeInOrder(BTNode T) {
+	/**
+     * 非递归实现中序遍历
+     */
+	public void iterativeInOrder(BTNode T) {
 		Stack<BTNode> stack = new Stack<>();
 		while (T != null || !stack.isEmpty()) {
 			if (T != null) {
@@ -78,8 +109,10 @@ public class BiTree {
 		}
 	}
 
-	/** 非递归实现后序遍历 */
-	protected static void iterativePostOrder(BTNode T) {
+	/**
+     * 非递归实现后序遍历
+     */
+	public void iterativePostOrder(BTNode T) {
 		Stack<BTNode> stack = new Stack<>();
 		Stack<Integer> helper = new Stack<>();
 		while (T != null || !stack.isEmpty()) {
@@ -101,8 +134,10 @@ public class BiTree {
 		}
 	}
 
-	/** 层次遍历 */
-	protected static void levelOrder(BTNode root) {
+	/**
+     * 层次遍历
+     */
+	public void levelOrder(BTNode root) {
 		if (root == null) {
 			return;
 		}
@@ -119,46 +154,15 @@ public class BiTree {
 		}
 	}
 
-	public static void main(String[] args) {
-		BiTree tree = new BiTree(init());
 
-		System.out.println("Pre-Order:");
-		preOrder(tree.getRoot());
-		System.out.println();
-		iterativePreOrder(tree.getRoot());
-		System.out.println();
 
-		System.out.println("In-Order:");
-		inOrder(tree.getRoot());
-		System.out.println();
-		iterativeInOrder(tree.getRoot());
-		System.out.println();
 
-		System.out.println("Post-Order:");
-		postOrder(tree.getRoot());
-		System.out.println();
-		iterativePostOrder(tree.getRoot());
-		System.out.println();
 
-		System.out.println("Level-Order:");
-		levelOrder(tree.getRoot());
-		System.out.println();
-	}
-
-	/** 构造树 */
-	public static BTNode init() {
-		BTNode a = new BTNode(1);
-		BTNode b = new BTNode(2, null, a);
-		BTNode c = new BTNode(3);
-		BTNode d = new BTNode(4, b, c);
-		BTNode e = new BTNode(5);
-		BTNode f = new BTNode(6, e, null);
-		BTNode g = new BTNode(7, null, f);
-		BTNode h = new BTNode(8, d, g);
-		return h; // root
-	}
 }
 
+/**
+ * 二叉树数据结构
+ */
 class BTNode {
 	private int val;
 	private BTNode left, right;
@@ -196,4 +200,34 @@ class BTNode {
 	public void setRight(BTNode right) {
 		this.right = right;
 	}
+}
+
+
+class TestBiTree {
+
+    public static void main(String[] args) {
+        BiTree tree = new BiTree(BiTree.init());
+
+        System.out.println("Pre-Order:");
+        tree.preOrder(tree.getRoot());
+        System.out.println();
+        tree.iterativePreOrder(tree.getRoot());
+        System.out.println();
+
+        System.out.println("In-Order:");
+        tree.inOrder(tree.getRoot());
+        System.out.println();
+        tree.iterativeInOrder(tree.getRoot());
+        System.out.println();
+
+        System.out.println("Post-Order:");
+        tree.postOrder(tree.getRoot());
+        System.out.println();
+        tree.iterativePostOrder(tree.getRoot());
+        System.out.println();
+
+        System.out.println("Level-Order:");
+        tree.levelOrder(tree.getRoot());
+        System.out.println();
+    }
 }
